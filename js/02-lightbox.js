635ad2574054de1a11751382db79640e392f0efd
M1keXl <mihayloh.com@gmail.com>
2023-03-13 22:16:40 +0200

    v1

---
+++ js/02-lightbox.js
@@ -0,0 +1,30 @@
+
+import { galleryItems } from './gallery-items.js';
+
+console.log(galleryItems);
+
+const gallery = document.querySelector(`.gallery`);
+const cardsMarkup = createGallery(galleryItems);
+gallery.insertAdjacentHTML(`beforeend`, cardsMarkup);
+
+function createGallery(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) =>
+    `<li>
+        <a class="gallery__item"
+            href="${original}"
+            onclick = "return false">
+        <img class="gallery__image"
+            src="${preview}" 
+            alt="${description}"/>
+        </a>
+        </li>`)
+    .join("");
+};
+
+const lightbox = new SimpleLightbox('.gallery a', { 
+    overlay : true,
+    overlayOpacity : 0.8,
+    captionsData : `alt`,
+    captionDelay: 250,
+    captionPosition: "bottom",
+});
